1f6d30a90c3a6d748424ca878b6b088d5e830509
bowserj <bowserj@665288e8-1556-0410-a889-83be2ee5b666>
2008-11-12 22:06:12 +0000

    We want to pause, not wait
    git-svn-id: http://completeui.googlecode.com/svn/trunk@23 665288e8-1556-0410-a889-83be2ee5b666

--- test/functional/client/calendar/selenium/remotecontrol/Test/RenderTest.java
+++ test/functional/client/calendar/selenium/remotecontrol/Test/RenderTest.java
@@ -20,7 +20,7 @@ public class RenderTest extends BaseTest {
     String hiddenDiv = eval("nitobi.getComponent('dp').getCalendar().getHtmlNode().className.split(\" \").length;");
     selenium.click("dp.button");
     // Firefox flies through this too fast, wait a second
-    wait("1000");
+    pause("1000");
     String shownDiv = eval("nitobi.getComponent('dp').getCalendar().getHtmlNode().className.split(\" \").length;");
     assertTrue(!hiddenDiv.equals(shownDiv));
   }
@@ -38,7 +38,7 @@ public class RenderTest extends BaseTest {
      */
     String next_month_id = eval("nitobi.html.getChildNodeByAttribute(nitobi.getComponent.('dp').getCalendar().getHtmlNode(), \"class\", \"ntb-calendar-next\", true).id;");
 		selenium.click(next_month_id);
-    wait("1000");
+    pause("1000");
     String endMonth = eval("nitobi.html.getChildNodeByAttribute(nitobi.getComponent.('dp').getCalendar().getHtmlNode(), \"class\", \"ntb-calendar-month\", true).innerHTML;");
     assertTrue(!startMonth.equals(endMonth));
     selenium.click("dp.button");
@@ -76,7 +76,7 @@ public class RenderTest extends BaseTest {
 		selenium.click("dp.button");
 		selenium.click("link=" + startMonth);
     // Wait a second for this to appear
-    wait("1000");
+    pause("1000");
     /* 
      * Need to get the month and the year of these selects
      */
@@ -90,7 +90,7 @@ public class RenderTest extends BaseTest {
     String confirmButton = eval("nitobi.html.getChildNodeByAttribute(nitobi.getComponent.('dp').getCalendar().getHtmlNode(), \"class\", \"ntb-calendar-controls\",true).childNodes[0].id;");
 		selenium.click(confirmButton);
 		selenium.click("link=12");
-    wait("1000");
+    pause("1000");
     String endDate = eval("nitobi.getComponent(\"dp\").getSelectedDate();");
     assertTrue(!startDate.equals(endDate));
   }
